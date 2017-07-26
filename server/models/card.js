43301165b959f310df9d520b8dc142d3f0010347
Darryl Kilzer <darrylkilzer@gmail.com>
2017-07-26 09:03:57 -0600

    updates to the schemas

--- server/models/card.js
+++ server/models/card.js
@@ -5,10 +5,16 @@ let ObjectId = mongoose.Schema.ObjectId
 var schema = new mongoose.Schema({
   title: { type: String, required: true },
   body: { type: String, required: true },
+  image: { type: String, required: true },
   type: [{ type: String, required: true }],
   users: [{ type: ObjectId, ref: models.user.name }],
   created: { type: Number, default: Date.now() },
-  foundCount: { type: Number, default: 0 }
+  foundCount: { type: Number, default: 0 },
+  pvp: { type: Boolean, default: true },
+  value: { type: Number, required: true },
+  lifespan: { type: Number},
+  action: { type: String },
+  cost: { type: String }
 });
 
 module.exports = mongoose.model(models.card.name, schema);
