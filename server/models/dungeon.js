18be2b6921b2bc8da4d5e800ae8b035b65e8b045
Darryl Kilzer <darrylkilzer@gmail.com>
2017-08-21 19:45:16 -0600

    working on dungeon models

---
+++ server/models/dungeon.js
@@ -0,0 +1,13 @@
+import { models } from '../config/constants'
+let mongoose = require('mongoose')
+let ObjectId = mongoose.Schema.ObjectId
+
+var schema = new mongoose.Schema({
+  title: { type: String, required: true },
+  story: { type: String, required: true },
+  creatorId: { type: ObjectId, ref: models.user.name, required: true },
+  created: { type: Number, default: Date.now() },
+  value: { type: Number }
+});
+
+module.exports = mongoose.model(models.deck.name, schema);
